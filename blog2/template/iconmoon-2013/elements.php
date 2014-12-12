cdf1480234806a2b92bd98c3a81b272c2a3b6c5a
JJ Ying <yingjunjiu@gmail.com>
2014-12-12 10:39:41 +0800

    Minor Update
    = Changed the name of the link to archive page

--- blog2/template/iconmoon-2013/elements.php
+++ blog2/template/iconmoon-2013/elements.php
@@ -89,7 +89,7 @@ $elements['displayside']=<<<eot
 </div>
 <div class="clear"></div>
 <div id='blog-side-about'>
-	图月志，界面设计个人博客，博主 <a href="../about" title="关于我">JJ Ying</a> 是<a href="http://mux.baidu.com">百度 MUX</a> 上海团队的负责人，平时经常出没于<a href="http://www.zhihu.com/people/jjying" title="我的知乎页面">知乎</a>、<a href="http://weibo.com/yingjunjiu/" title="我的新浪微博">新浪微博</a>和 <a href="http://dribbble.com/players/JJYing" title="我的 Dribbble 页面">Dribbble</a> 。<br/><br/>浏览过往博文请移步<a href="archive.php">归档页</a>，本博客所有内容若需转载请<a href="mailto:ying.jun.jiu@gmail.com" title="我的电子邮件">联系我</a>。<br/><br/>订阅图月志：<a href="http://pic.yupoo.com/yingjunjiu_v/DI8qiPwg/WUeB.jpg">微信</a> / <a href="http://eepurl.com/GR6Gv">E-mail</a> / <a href="http://feed.feedsky.com/yingjunjiu">RSS</a>
+	图月志，界面设计个人博客，博主 <a href="../about" title="关于我">JJ Ying</a> 是<a href="http://mux.baidu.com">百度 MUX</a> 上海团队的负责人，平时经常出没于<a href="http://www.zhihu.com/people/jjying" title="我的知乎页面">知乎</a>、<a href="http://weibo.com/yingjunjiu/" title="我的新浪微博">新浪微博</a>和 <a href="http://dribbble.com/players/JJYing" title="我的 Dribbble 页面">Dribbble</a> 。<br/><br/>浏览过往博文请移步<a href="archive.php">历史热文榜</a>，本博客所有内容若需转载请<a href="mailto:ying.jun.jiu@gmail.com" title="我的电子邮件">联系我</a>。<br/><br/>订阅图月志：<a href="http://pic.yupoo.com/yingjunjiu_v/DI8qiPwg/WUeB.jpg">微信</a> / <a href="http://eepurl.com/GR6Gv">E-mail</a> / <a href="http://feed.feedsky.com/yingjunjiu">RSS</a>
 </div>
 
 <div class="clear"></div>
