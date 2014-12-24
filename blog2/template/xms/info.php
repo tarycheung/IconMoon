976100425a0d879f7b3c499660803f82ddb2aa10
JJ Ying <yingjunjiu@gmail.com>
2014-12-24 19:44:42 +0800

    X'mas Template
    + Added a template for X’mas

---
+++ blog2/template/xms/info.php
@@ -0,0 +1,10 @@
+<?PHP
+$template['id']="xms";
+$template['name']="xms";
+$template['author']="JJ Ying";
+$template['intro']="Designed by JJ.Ying     http://IconMoon.com";
+$template['dirname']="xms";
+$template['thumbnail']="thumb.jpg";
+$template['structure']="template/xms/elements.php";
+$template['images']="template/xms/images";
+$template['css'][0]="template/xms/styles.css";
